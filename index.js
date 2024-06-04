aea8d2db14c97fe7beeed8f0b42132407d1d3f7f
joeya <joeyanegbe@icloud.com>
2024-06-04 15:26:49 +0100

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT
 
 // adding middleware
 app.use(express.json())
+app.use(cors())
 
 const characters=require("./harrypotter.json")
 
